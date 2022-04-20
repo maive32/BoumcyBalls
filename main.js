639d63250c42018ad111e5bdc5fa0fec48bc0ba1
maive32 <94019913+maive32@users.noreply.github.com>
2022-04-20 12:45:59 -0700

    f

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ Ball.prototype.draw = function() {
   ctx.fillStyle = this.color; 
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
   ctx.fill();
-
+}
 // updating the ball's data 
 
 Ball.prototype.update = function() {
@@ -74,6 +74,7 @@ while (balls.length < 25) {
 }
 
 // loops the new instance of Ball()
+
 function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
   ctx.fillRect(0, 0, width, height);
@@ -81,25 +82,26 @@ function loop() {
   for (let i = 0; i < balls.length; i++) {
     balls[i].draw();
     balls[i].update();
+    Ball.prototype.update
+    Ball.prototype.collisionDetect = function() {
+      for (let j = 0; j < balls.length; j++) {
+        if (!(this === balls[j])) {
+          const dx = this.x - balls[j].x;
+          const dy = this.y - balls[j].y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+    
+          if (distance < this.size + balls[j].size) {
+            balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+          }
+        }
+      }
+    }
+    balls[i].update();
     balls[i].collisionDetect();
   }
 
   requestAnimationFrame(loop);
 }
 
-// adding collision detection 
 
-Ball.prototype.collisionDetect = function() {
-  for (let j = 0; j < balls.length; j++) {
-    if (!(this === balls[j])) {
-      const dx = this.x - balls[j].x;
-      const dy = this.y - balls[j].y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < this.size + balls[j].size) {
-        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
-      }
-    }
-  }
-}
-
+loop();
